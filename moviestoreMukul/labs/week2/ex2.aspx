e67a41b1ba9c7aed949104e8eef29e735666c033
Raxxit <raxxit004@gmail.com>
2025-05-12 19:23:01 +0400

    completed lab exercise2

---
+++ moviestoreMukul/labs/week2/ex2.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/guest.Master" AutoEventWireup="true" CodeBehind="ex2.aspx.cs" Inherits="moviestoreMukul.labs.week2.ex2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="headcontent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="maincontent" runat="server">
+
+    <asp:Label ID="lblmessage" runat="server" Text=""></asp:Label>
+    <asp:Button ID="btnshow" OnClick="btnshow_Click" runat="server" Text="show" />
+    <asp:Button ID="btnhide" OnClick="btnhide_Click" runat="server" Text="hide" />
+
+</asp:Content>
---
+++ moviestoreMukul/labs/week2/ex2.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace moviestoreMukul.labs.week2
+{
+    public partial class ex2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblmessage.Text = "Now you see me";
+        }
+
+        protected void btnshow_Click(object sender, EventArgs e)
+        {
+            lblmessage.Visible = true;
+        }
+
+        protected void btnhide_Click(object sender, EventArgs e)
+        {
+            lblmessage.Visible = false;
+        }
+    }
+}
